9f50ced5b39165a6b7a798f53936ea6c58cac055
Justin Hong <abigsmall@gmail.com>
2020-12-13 22:54:05 -0800

    Add Flight Booker WIP

--- src/sevenguis/components/flight_booker.cljs
+++ src/sevenguis/components/flight_booker.cljs
@@ -3,4 +3,17 @@
 
 (defn flight-booker
   []
-  [:p "Flight Booker"])
+  [:div
+   [:h2 "Flight Booker"]
+   [:select
+    {:name "Flight type"}
+    [:option
+     {:value "one-way"}
+     "one-way flight"]
+    [:option
+     {:value "return"}
+     "return flight"]]
+   [:input
+    {:value "new Date() JS interop"}]
+   [:input
+    {:value "new Date() JS interop"}]])
