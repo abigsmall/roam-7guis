bd784cff28292d7e94434f859250623c6c6ac395
Justin Hong <abigsmall@gmail.com>
2020-12-13 22:43:15 -0800

    Add Flight Booker skeleton

---
+++ src/sevenguis/components/flight_booker.cljs
@@ -0,0 +1,6 @@
+(ns sevenguis.components.flight-booker
+  (:require [reagent.core :as r]))
+
+(defn flight-booker
+  []
+  [:p "Flight Booker"])
--- src/sevenguis/core.cljs
+++ src/sevenguis/core.cljs
@@ -1,13 +1,15 @@
 (ns sevenguis.core
   (:require [reagent.core :as r]
             [sevenguis.components.counter :refer [counter]]
-            [sevenguis.components.temperature-converter :refer [temperature-converter]]))
+            [sevenguis.components.temperature-converter :refer [temperature-converter]]
+            [sevenguis.components.flight-booker :refer [flight-booker]]))
 
 (defn app
   []
   [:div
    [counter]
-   [temperature-converter]])
+   [temperature-converter]
+   [flight-booker]])
 
 (defn ^:export main
   []
