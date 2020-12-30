c9285db941f44d688a6079c06fe6679ca21ecca6
Justin Hong <abigsmall@gmail.com>
2020-12-30 13:19:04 -0800

    Remove old Temperature Converter code

--- src/sevenguis/components/temperature_converter.cljs
+++ src/sevenguis/components/temperature_converter.cljs
@@ -9,18 +9,6 @@
   [f]
   (* (- f 32) (/ 5 9)))
 
-(defn validate-old
-  [input]
-  (let [number (js/Number input)
-        parsed-float (js/parseFloat input)
-        valid (and (not (js/isNaN number)) (not (js/isNaN parsed-float)))]
-    (.log js/console "validate: input:" input)
-    (.log js/console "validate: (= input \"\"):" (= input ""))
-    (.log js/console "validate: parsed-float:" parsed-float)
-    (.log js/console "validate: valid:" valid)
-    {:input input
-     :valid valid}))
-
 (defn validate
   [input]
   (let [number (js/Number input)
@@ -31,48 +19,6 @@
       "" true
       valid)))
 
-; (defn validated-input
-;   [{:keys [label change-handler]}]
-;   (let [valid (r/atom true)]
-;     (fn
-;       [{:keys [value]}]
-;       [:label
-;        [:input
-;         {:class (when-not @valid "invalid")
-;          :value value
-;          :on-change (fn [e]
-;                       (let [{valid? :valid input :input} (validate (.. e -target -value))]
-;                         ; (.log js/console "validated-input: on-change: valid?:" valid?)
-;                         ; (.log js/console "validated-input: on-change: input:" input)
-;                         ; (.log js/console "validated-input: on-change: @valid:" @valid)
-;                         (when (not= valid? @valid) (reset! valid valid?))
-;                         (change-handler input valid?)))}]
-;        label])))
-
-; (defn temperature-converter
-;   []
-;   (let [c (r/atom nil)
-;         f (r/atom nil)]
-;     (fn
-;       []
-;       [:div
-;        [:h2 "Temperature Converter"]
-;        [validated-input
-;         {:label "Fahrenheit"
-;          :value @f
-;          :change-handler (fn [input valid?]
-;                            (reset! f input)
-;                            (cond
-;                              (= input "") (reset! c nil)
-;                              valid? (reset! c (f->c input))
-;                              :else (reset! c nil)))}]
-;
-;        [validated-input
-;         {:label "Celsius"
-;          :value @c
-;          :change-handler (fn [input]
-;                            (reset! c input)
-;                            (reset! f (c->f input)))}]])))
 (defn empty-input?
   [input]
   (cond
@@ -94,6 +40,8 @@
                    (not (validate @f)) "invalid"
                    (empty-input? @f) nil
                    (not (validate @c)) "decoupled")
+                   ; (or (not (validate @c)) (empty-input? @c)) "decoupled" ;; or c has empty input
+
           :value @f
           :on-change (fn [e]
                        (let [input (.. e -target -value)]
@@ -113,4 +61,5 @@
                        (let [input (.. e -target -value)]
                          (reset! c input)
                          (when (validate @c)
-                           (reset! f (c->f input)))))}]]])))
+                           (reset! f (c->f input)))))}]
+        "Celsius"]])))
