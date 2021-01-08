9e23d1509df6b30d33e29eeda824a6e3961f9647
Justin Hong <abigsmall@gmail.com>
2021-01-07 21:03:07 -0800

    Simplify empty-input?

--- src/sevenguis/components/temperature_converter.cljs
+++ src/sevenguis/components/temperature_converter.cljs
@@ -18,20 +18,13 @@
 
 (defn empty-input?
   [input]
-  (cond
-    (nil? input) true
-    (= input "") true
-    :else false))
-
-(defn valid-input?
-  [input]
-  (or (valid-number? input) (empty-input? input)))
+  (or (nil? input) (= "" input)))
 
 (defn temperature-converter
   []
   (let [c (r/atom nil)
         f (r/atom nil)]
-    (fn                 ;; does this have to be a function? can’t it be just a hiccup element?
+    (fn
       []
       [:div
        [:h2 "Temperature Converter2"]
@@ -41,16 +34,13 @@
                    (empty-input? @f) nil
                    (not (valid-number? @f)) "invalid"
                    (or (not (valid-number? @c)) (empty-input? @c)) "decoupled")
-                   ; (or (not (validate @c)) (empty-input? @c)) "decoupled" ;; or c has empty input
-
           :value @f
           :on-change (fn [e]
                        (let [input (.. e -target -value)]
                          (reset! f input)
                          (when (valid-number? input)
-                           ; if input is not nil or empty string and is valid number
+                             ; if input is not nil or empty string and is valid number
                            (reset! c (f->c input)))))}]
-
         "Fahrenheit"]
        [:label
         [:input
