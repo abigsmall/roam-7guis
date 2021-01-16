0f2b272de4a4a8c1359e36bd0d03bf5525625b61
Justin Hong <abigsmall@gmail.com>
2021-01-15 18:06:17 -0800

    Add Flight Booker date validation

--- src/sevenguis/components/flight_booker.cljs
+++ src/sevenguis/components/flight_booker.cljs
@@ -1,19 +1,47 @@
 (ns sevenguis.components.flight-booker
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [reagent.ratom :as ratom]))
+
+(def date-regex #"\d{4}-\d{2}-(\d{2})")
+
+(defn validate-date
+  [date]
+  (let [[match-result captured-day] (re-matches date-regex date)]
+    (if match-result
+      (when (= (js/parseInt captured-day) (.getUTCDate (js/Date. match-result)))
+        match-result)
+      match-result)))
 
 (defn flight-booker
   []
-  [:div
-   [:h2 "Flight Booker"]
-   [:select
-    {:name "Flight type"}
-    [:option
-     {:value "one-way"}
-     "one-way flight"]
-    [:option
-     {:value "return"}
-     "return flight"]]
-   [:input
-    {:value "new Date() JS interop"}]
-   [:input
-    {:value "new Date() JS interop"}]])
+  (let [c (r/atom :one-way)
+        t1 (r/atom {:value "2021-01-08"})
+        t2 (r/atom {:value "2021-01-08"
+                    :enabled (ratom/reaction (= :round @c))})]
+    (fn
+      []
+      [:div
+       [:select
+        {:name "Flight type"
+         :on-change (fn [e]
+                      (let [selection (.. e -target -value)]
+                        (reset! c (keyword selection))))}
+        [:option
+         {:value "one-way"}
+         "one-way flight"]
+        [:option
+         {:value "return"}
+         "return flight"]]
+       [:input
+        {:class (when-not (validate-date (:value @t1)) "invalid")
+         :value (:value @t1)
+         :on-change (fn [e]
+                      (let [input (.. e -target -value)]
+                        (swap! t1 assoc :value input)))}]
+       [:input
+        {:class (when-not (validate-date (:value @t2)) "invalid")
+         :value (:value @t2)
+         :on-change (fn [e]
+                      (let [input (.. e -target -value)]
+                        (swap! t2 assoc :value input)))}]
+       [:button "Book"]])))
